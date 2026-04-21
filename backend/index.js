9214268e29a5a33fade871fdc068cea0e9181cbb
Krish <krishgabani08@gmail.com>
2026-04-21 12:35:52 +0530

    port changes

--- backend/index.js
+++ backend/index.js
@@ -53,6 +53,6 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('✅ MongoDB connected!'))
   .catch((err) => console.log('Connection error:', err))
 
-app.listen(process.env.PORT, () => {
-  console.log(`🛜  Server running on port ${process.env.PORT}`)
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`🛜  Server running on port ${process.env.PORT || 5000}`)
 })
